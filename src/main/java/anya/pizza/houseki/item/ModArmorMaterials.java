02bc666ef0543b2d157cf6c86e1f8c50fee071c9
AnyaPizza <axolotlanya18@gmail.com>
2026-04-10 19:07:10 -0400

    Fix durability of armors.

--- src/main/java/anya/pizza/houseki/item/ModArmorMaterials.java
+++ src/main/java/anya/pizza/houseki/item/ModArmorMaterials.java
@@ -26,7 +26,7 @@ public class ModArmorMaterials {
     public static final RegistryKey<EquipmentAsset> CAST_STEEL_KEY = RegistryKey.of(REGISTRY_KEY, Identifier.of(Houseki.MOD_ID, "cast_steel"));
     public static final RegistryKey<EquipmentAsset> METEORIC_IRON_KEY = RegistryKey.of(REGISTRY_KEY, Identifier.of(Houseki.MOD_ID, "meteoric_iron"));
 
-    public static final ArmorMaterial PINKU_MATERIAL = new ArmorMaterial(2580, Util.make(new EnumMap<>(EquipmentType.class), map -> {
+    public static final ArmorMaterial PINKU_MATERIAL = new ArmorMaterial(47, Util.make(new EnumMap<>(EquipmentType.class), map -> {
         map.put(EquipmentType.HELMET, 6);
         map.put(EquipmentType.CHESTPLATE, 11);
         map.put(EquipmentType.LEGGINGS, 9);
@@ -34,7 +34,7 @@ public class ModArmorMaterials {
         map.put(EquipmentType.BODY, 14);
     }), 16, SoundEvents.ITEM_ARMOR_EQUIP_NETHERITE, 5, 0.3f, ModTags.Items.PINKU_REPAIR, PINKU_KEY);
 
-    public static final ArmorMaterial RAINBOW_MATERIAL = new ArmorMaterial(2000, Util.make(new EnumMap<>(EquipmentType.class), map -> {
+    public static final ArmorMaterial RAINBOW_MATERIAL = new ArmorMaterial(16, Util.make(new EnumMap<>(EquipmentType.class), map -> {
         map.put(EquipmentType.HELMET, 3);
         map.put(EquipmentType.CHESTPLATE, 8);
         map.put(EquipmentType.LEGGINGS, 6);
@@ -42,7 +42,7 @@ public class ModArmorMaterials {
         map.put(EquipmentType.BODY, 11);
     }), 11, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 2.5f, 0.1f, ModTags.Items.RAINBOW_REPAIR, RAINBOW_PYRITE_KEY);
 
-    public static final ArmorMaterial TUNGSTEN_MATERIAL = new ArmorMaterial(3000, Util.make(new EnumMap<>(EquipmentType.class), map -> {
+    public static final ArmorMaterial TUNGSTEN_MATERIAL = new ArmorMaterial(38, Util.make(new EnumMap<>(EquipmentType.class), map -> {
         map.put(EquipmentType.HELMET, 4);
         map.put(EquipmentType.CHESTPLATE, 9);
         map.put(EquipmentType.LEGGINGS, 7);
@@ -50,7 +50,7 @@ public class ModArmorMaterials {
         map.put(EquipmentType.BODY, 12);
     }), 15, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 4, 0.2f, ModTags.Items.TUNGSTEN_REPAIR, TUNGSTEN_KEY);
 
-    public static final ArmorMaterial ALUMINUM_MATERIAL = new ArmorMaterial(1500, Util.make(new EnumMap<>(EquipmentType.class), map -> {
+    public static final ArmorMaterial ALUMINUM_MATERIAL = new ArmorMaterial(15, Util.make(new EnumMap<>(EquipmentType.class), map -> {
         map.put(EquipmentType.HELMET, 3);
         map.put(EquipmentType.CHESTPLATE, 8);
         map.put(EquipmentType.LEGGINGS, 6);
@@ -58,7 +58,7 @@ public class ModArmorMaterials {
         map.put(EquipmentType.BODY, 10);
     }), 11, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 2.5f, 0.1f, ModTags.Items.ALUMINUM_REPAIR, ALUMINUM_KEY);
 
-    public static final ArmorMaterial SAPPHIRE_MATERIAL = new ArmorMaterial(3000, Util.make(new EnumMap<>(EquipmentType.class), map -> {
+    public static final ArmorMaterial SAPPHIRE_MATERIAL = new ArmorMaterial(34, Util.make(new EnumMap<>(EquipmentType.class), map -> {
         map.put(EquipmentType.HELMET, 5);
         map.put(EquipmentType.CHESTPLATE, 10);
         map.put(EquipmentType.LEGGINGS, 8);
@@ -66,7 +66,7 @@ public class ModArmorMaterials {
         map.put(EquipmentType.BODY, 13);
     }), 12, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 4.5f, 0.3f, ModTags.Items.SAPPHIRE_REPAIR, SAPPHIRE_KEY);
 
-    public static final ArmorMaterial NEPHRITE_MATERIAL = new ArmorMaterial(2000, Util.make(new EnumMap<>(EquipmentType.class), map -> {
+    public static final ArmorMaterial NEPHRITE_MATERIAL = new ArmorMaterial(33, Util.make(new EnumMap<>(EquipmentType.class), map -> {
         map.put(EquipmentType.HELMET, 3);
         map.put(EquipmentType.CHESTPLATE, 9);
         map.put(EquipmentType.LEGGINGS, 6);
@@ -74,7 +74,7 @@ public class ModArmorMaterials {
         map.put(EquipmentType.BODY, 11);
     }), 11, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 3.5f, 0.3f, ModTags.Items.NEPHRITE_REPAIR, NEPHRITE_KEY);
 
-    public static final ArmorMaterial JADEITE_MATERIAL = new ArmorMaterial(2500, Util.make(new EnumMap<>(EquipmentType.class), map -> {
+    public static final ArmorMaterial JADEITE_MATERIAL = new ArmorMaterial(36, Util.make(new EnumMap<>(EquipmentType.class), map -> {
         map.put(EquipmentType.HELMET, 4);
         map.put(EquipmentType.CHESTPLATE, 10);
         map.put(EquipmentType.LEGGINGS, 7);
@@ -82,7 +82,7 @@ public class ModArmorMaterials {
         map.put(EquipmentType.BODY, 12);
     }), 11, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 4, 0.4f, ModTags.Items.JADEITE_REPAIR, JADEITE_KEY);
 
-    public static final ArmorMaterial PLATINUM_MATERIAL = new ArmorMaterial(3000, Util.make(new EnumMap<>(EquipmentType.class), map -> {
+    public static final ArmorMaterial PLATINUM_MATERIAL = new ArmorMaterial(35, Util.make(new EnumMap<>(EquipmentType.class), map -> {
         map.put(EquipmentType.HELMET, 4);
         map.put(EquipmentType.CHESTPLATE, 9);
         map.put(EquipmentType.LEGGINGS, 7);
@@ -90,7 +90,7 @@ public class ModArmorMaterials {
         map.put(EquipmentType.BODY, 12);
     }), 15, SoundEvents.ITEM_ARMOR_EQUIP_GOLD, 4, 0.2f, ModTags.Items.PLATINUM_REPAIR, PLATINUM_KEY);
 
-    public static final ArmorMaterial CAST_STEEL_MATERIAL = new ArmorMaterial(3000, Util.make(new EnumMap<>(EquipmentType.class), map -> {
+    public static final ArmorMaterial CAST_STEEL_MATERIAL = new ArmorMaterial(35, Util.make(new EnumMap<>(EquipmentType.class), map -> {
         map.put(EquipmentType.HELMET, 4);
         map.put(EquipmentType.CHESTPLATE, 9);
         map.put(EquipmentType.LEGGINGS, 7);
@@ -98,7 +98,7 @@ public class ModArmorMaterials {
         map.put(EquipmentType.BODY, 12);
     }), 15, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 4.5f, 0.3f, ModTags.Items.CAST_STEEL_REPAIR, CAST_STEEL_KEY);
 
-    public static final ArmorMaterial METEORIC_IRON_MATERIAL = new ArmorMaterial(3000, Util.make(new EnumMap<>(EquipmentType.class), map -> {
+    public static final ArmorMaterial METEORIC_IRON_MATERIAL = new ArmorMaterial(38, Util.make(new EnumMap<>(EquipmentType.class), map -> {
         map.put(EquipmentType.HELMET, 4);
         map.put(EquipmentType.CHESTPLATE, 9);
         map.put(EquipmentType.LEGGINGS, 7);
